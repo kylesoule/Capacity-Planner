f9744110aee236863e38983efd030220732b70b2
Kyle Soule <kyle.soule@gmail.com>
2017-01-31 22:32:30 -0600

    Working GA

--- Genetic Algorithm.py
+++ Genetic Algorithm.py
@@ -32,7 +32,7 @@ lup = { "0001": 1,
         "1100": "*",
         "1101": "/"}
 
-def fitness_proportionate_selection(choices):
+def fitness_proportionate_selection():
     max = sum(choices.values())
     pick = random.uniform(0, max)
     current = 0
@@ -41,69 +41,100 @@ def fitness_proportionate_selection(choices):
         if current > pick:
             return key
 
-def random_seed(size):
-    # Only returns valid form seeds
+def new_seed(size):
+    # Generates a new seed
     seed = ""
     for i in xrange(0, size):
         if i == 0:
             seed = pop[random.randint(0, len(pop) - 1)]
         else:
             seed = seed + " " + pop[random.randint(0, len(pop) - 1)]
-    
-    if is_form_valid(seed):
-        return seed
-    else:
-        random_seed(size)
+    return seed
+
+def random_seed(size):
+    # Only returns valid form seeds
+    while True:
+        seed = new_seed(size)
+        if is_form_valid(seed):
+            break
+    return seed
 
 def is_form_valid(member):
     # Expected form is value, operator, ...
-    valid = True
     parts = member.split(" ")
     for i in xrange(0, len(parts)):
-        if i % 2 == 1:
-            if int(parts[i], 2) < 10:
-                valid = False
-        else:
-            if int(parts[i], 2) > 9:
-                valid = False
-    return valid
+        val = int(parts[i], 2)
+        if i % 2 == 1:  # operator
+            if val < 10 or val > 13:
+                return False
+        else: # value
+            if val == 0 or val > 9:
+                return False
+    return True
 
 def evaluate(member):
     parts = member.split(" ")
     p_val = int(parts[0], 2)
     val = 0.0 + p_val
     op = 0
-
-    for i in xrange(0, len(parts)):
-        if i % 2 == 1:
-            op = int(parts[i], 2)
-        else:
-            p = int(parts[i], 2)
-            if op == 10:
-                val += p
-            elif op == 11:
-                val -= p
-            elif op == 12:
-                val *= p
-            elif op == 13:
-                val /= p
+    
+    try:
+        for i in xrange(0, len(parts)):
+            if i % 2 == 1:
+                op = int(parts[i], 2)
+            else:
+                p = int(parts[i], 2)
+                if op == 10:
+                    val += p
+                elif op == 11:
+                    val -= p
+                elif op == 12:
+                    val *= p
+                elif op == 13:
+                    val /= p
+    except:
+        return 0
+    
     return val
 
 def breed(member):
     rand_xover = random.uniform(0, 1)
-    rand_mutate = random.uniform(0, 1)
+    member_list = list(member)
     
     if rand_xover <= crossover_rate:
-        xover_start = random.randint(0, len(member))
-        
-        for i in xrange(0, len(member) - 1):
-            if member[i] != " ":
+        xover_start = random.randint(0, len(member_list))
+        for i in xrange(0, len(member_list)):
+            rand_mutate = random.uniform(0, 1)
+            
+            if member_list[i] != " ":
                 if i >= xover_start or rand_mutate <= mutation_rate:
-                    if member[i] == "0":
-                        member[i] = "1"
+                    if member_list[i] == "0":
+                        member_list[i] = "1"
                     else:
-                        member[i] = "0"
-    return member
+                        member_list[i] = "0"
+        
+        member = "".join(member_list)
+
+        if is_form_valid(member):
+            if evaluate(member_a) != criteria:
+                choices[member] = 1 / abs((evaluate(member_a) - criteria))
+    else:
+        for i in xrange(0, len(member_list)):
+            rand_mutate = random.uniform(0, 1)
+            
+            if member_list[i] != " ":
+                if rand_mutate <= mutation_rate:
+                    if member_list[i] == "0":
+                        member_list[i] = "1"
+                    else:
+                        member_list[i] = "0"
+        
+        member = "".join(member_list)
+        if is_form_valid(member):
+            if evaluate(member_a) != criteria:
+                choices[member] = 1 / abs((evaluate(member_a) - criteria))
+    
+    return fitness_proportionate_selection()
             
 
 # Genetic algorithm
@@ -111,16 +142,29 @@ member_a = random_seed(9)
 criteria = 23
 crossover_rate = 0.7
 mutation_rate = 0.001
+choices = {member_a: 1 / abs((evaluate(member_a) - criteria))}
+solved = 0
+f = open("C:\\Users\\namestaken\\Documents\\GitHub\\Capacity-Planner\\benchmark.csv", "w")
 
 while True:
     if evaluate(member_a) == criteria:
-        print member_a
-        for pc in member_a.split(" "):
-            sys.stdout.write("{p} ".format(p=lup[pc]))
-        sys.stdout.write("= {s}\n".format(s=criteria))
-    else:
-        member = breed(member_a)
+        #print member_a
+        #for pc in member_a.split(" "):
+        #    sys.stdout.write("{p} ".format(p=lup[pc]))
+        #sys.stdout.write("= {s}\n".format(s=criteria))
         
+        f.write("{time}\n".format(time=(datetime.now() - startTime)))
+        startTime = datetime.now()
+        
+        if solved == 100:
+            break
+        member_a = breed(member_a)
+        solved += 1
+    else:
+        member_a = breed(member_a)
+        
+f.close()
+
 # Brute force
 '''
 while True:
@@ -138,4 +182,4 @@ while True:
 # Benchmarks: 0.9s, 5.1s, 1.5s, 4.9s
 '''
 
-print "Execution time: {t}".format(t = datetime.now() - startTime)
+#print "Execution time: {t}".format(t = datetime.now() - startTime)
