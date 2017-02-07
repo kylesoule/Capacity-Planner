32d8cf61ac6a6f80e216992bfea863e259573757
Kyle Soule <kyle.soule@gmail.com>
2017-02-06 21:43:53 -0600

    Added Functionality to TS GA
    Need to implement some type of ordered system...python dictionaries are
    not ordered.

--- Traveling Salesman.py
+++ Traveling Salesman.py
@@ -6,15 +6,14 @@ from datetime import datetime
 startTime = datetime.now()
 
 
-class Tour:
-    cities = {}
-    
+class Tour(object):
     def __init__(self):
+        self.cities = {}
         for i in xrange(0, len(city_list)):
-            self.cities["C{city}".format(city=len(self.cities))] = [None, None]
+            self.cities["C{city}".format(city=len(self.cities.keys()))] = [None, None]
     
-    def parseCities(self, city_list):
-        for city in city_list.split(" "):
+    def parseCities(self, cities):
+        for city in cities.split(" "):
             x, y = city_list[city]
             self.addCityByName(city, x, y)
     
@@ -33,7 +32,10 @@ class Tour:
                 self.cities["C{city}".format(city=i)] = [x, y]
     
     def getCityByPlace(self, place):
-        return self.cities.get(place)
+        return self.cities.items()[place]
+    
+    def getCityCoordByPlace(self, place):
+        return self.cities.values()[place]
 
     def getCityByName(self, city):
         if city[:1] != "C":
@@ -70,9 +72,128 @@ def begin_touring():
     parent2 = fitness_proportionate_selection()
     
     child = crossover(parent1, parent2)
+    child = mutate(child)
+    
+    for key, [x,y] in child.cities.items():
+        print "{key}({x},{y})".format(key=key, x=x, y=y)
 
 def crossover(parent1, parent2):
     child = Tour()
+    p1 = Tour()
+    p2 = Tour()
+    p1.parseCities(parent1)
+    p2.parseCities(parent2)
+    start = random.randint(0, p1.getSize())
+    end = random.randint(0, p1.getSize())
+    
+    if start > end:
+        temp = end
+        end = start
+        start = temp
+
+    for i in xrange(start, end):
+        name, [x,y] = p1.getCityByPlace(i)
+        child.addCityByName(name, x, y)
+    
+    for i in xrange(0, p2.getSize()):
+        if child.getCityCoordByPlace(i) == [None,None]:
+            name, [x,y] = p2.getCityByPlace(i)
+            child.addCityByName(name, x, y)
+            
+    for key, [x,y] in p1.cities.items():
+        print "{key}({x},{y})".format(key=key, x=x, y=y)
+    print "\n\n"
+    for key, [x,y] in p2.cities.items():
+        print "{key}({x},{y})".format(key=key, x=x, y=y)
+    print "\n\n"
+
+    return child
+
+def mutate(child):
+    for i in xrange(0, child.getSize() - 1):
+        if random.random() <= mutation_rate:
+            name1, [x1,y1] = child.getCityByPlace(i)
+            name2, [x2,y2] = child.getCityByPlace(i + 1)
+            child.addCityByName(name1, x2, y2)
+            child.addCityByName(name2, x1, y1)
+    return child
+
+
+
+mutation_rate = 0.01
+
+city_list = { "C0": [20  , 20]  ,
+              "C1": [60  , 20]  ,
+              "C2": [20  , 40]  ,
+              "C3": [40  , 120] ,
+              "C4": [60  , 80]  ,
+              "C5": [60  , 200] ,
+              "C6": [100 , 120] ,
+              "C7": [80  , 180] ,
+              "C8": [140 , 140] ,
+              "C9": [20  , 160] ,
+             "C10": [100 , 160] ,
+             "C11": [120 , 80]  ,
+             "C12": [140 , 180] ,
+             "C13": [180 , 60]  ,
+             "C14": [180 , 200] ,
+             "C15": [160 , 20]  ,
+             "C16": [180 , 100] ,
+             "C17": [100 , 40]  ,
+             "C18": [200 , 160] ,
+             "C19": [200 , 40]  }
+
+tour = Tour()
+
+for key, val in city_list.items():
+    x, y = val
+    tour.addCity(x, y)
+
+best = ["START", 0]
+choices = {tour.encodeTour(): tour.getFitness()}
+
+test = 0
+
+while True:
+    if test == 1:
+        #print best
+        break
+    else:
+        begin_touring()
+    test += 1
+
+'''
+# Genetic algorithm
+stack_a = random_seed(10)
+
+solution_a = 36
+solution_b = 360
+
+crossover_rate = 0.2
+mutation_rate = 0.7
+
+choices = {stack_a: 1.0 / evaluate(stack_a)[0]}
+
+runs = 0
+
+while True:
+    if evaluate(stack_a)[0] == 0:
+        print stack_a
+        #for i in xrange(0, len(stack_a)):
+        #    sys.stdout.write("{p} ".format(p=(i + 1)))
+        #sys.stdout.write("= {s}\n".format(s=0))
+        
+        startTime = datetime.now()
+        break
+    else:
+        stack_a = breed(stack_a)
+    #runs += 1
+    #if runs == 1000:
+    #    for key, val in choices.items():
+    #        print "{k}\t{v}".format(k=key, v=val)
+    #    break
+
+print "Execution time: {t}".format(t = datetime.now() - startTime)
 
 def new_seed(size):
     seed = [None]*size
@@ -160,81 +281,4 @@ def breed(member):
                 choices[member] = 1.0
 
     return fitness_proportionate_selection()
-
-
-crossover_rate = 0.7
-mutation_rate = 0.1
-
-city_list = { "C0": [20  , 20]  ,
-              "C1": [60  , 20]  ,
-              "C2": [20  , 40]  ,
-              "C3": [40  , 120] ,
-              "C4": [60  , 80]  ,
-              "C5": [60  , 200] ,
-              "C6": [100 , 120] ,
-              "C7": [80  , 180] ,
-              "C8": [140 , 140] ,
-              "C9": [20  , 160] ,
-             "C10": [100 , 160] ,
-             "C11": [120 , 80]  ,
-             "C12": [140 , 180] ,
-             "C13": [180 , 60]  ,
-             "C14": [180 , 200] ,
-             "C15": [160 , 20]  ,
-             "C16": [180 , 100] ,
-             "C17": [100 , 40]  ,
-             "C18": [200 , 160] ,
-             "C19": [200 , 40]  }
-
-tour = Tour()
-
-for key, val in city_list.items():
-    x, y = val
-    tour.addCity(x, y)
-
-best = ["START", 0]
-choices = {tour.encodeTour(): tour.getFitness()}
-
-test = 0
-
-while True:
-    if test == 1000:
-        print best
-        break
-    else:
-        begin_touring()
-    test += 1
-
-'''
-# Genetic algorithm
-stack_a = random_seed(10)
-
-solution_a = 36
-solution_b = 360
-
-crossover_rate = 0.2
-mutation_rate = 0.7
-
-choices = {stack_a: 1.0 / evaluate(stack_a)[0]}
-
-runs = 0
-
-while True:
-    if evaluate(stack_a)[0] == 0:
-        print stack_a
-        #for i in xrange(0, len(stack_a)):
-        #    sys.stdout.write("{p} ".format(p=(i + 1)))
-        #sys.stdout.write("= {s}\n".format(s=0))
-        
-        startTime = datetime.now()
-        break
-    else:
-        stack_a = breed(stack_a)
-    #runs += 1
-    #if runs == 1000:
-    #    for key, val in choices.items():
-    #        print "{k}\t{v}".format(k=key, v=val)
-    #    break
-
-print "Execution time: {t}".format(t = datetime.now() - startTime)
 '''
